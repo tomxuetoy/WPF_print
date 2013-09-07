21609ef55014d27758b565a174d36401554b6ccb
Formal codes goto: tomxue <tomxue@outlook.com>
2013-09-07 12:01:32 +0800

    C# print numbers from 0 to 100

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+using System;
+class Program
+{
+    static void Main()
+    {
+        // Write an int with Console.WriteLine.
+        int i;
+        for (i = 0; i < 1000; i++)
+            Console.WriteLine(i);
+    }
+}
